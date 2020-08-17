15ab0b099b64a50944af206fe2db7ceecf0e5476
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-17 18:00:36 +0530

    Day-01 complete

--- 01-JS/add.js
+++ 01-JS/add.js
@@ -17,6 +17,16 @@ function add(){
     return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add(Array.prototype.slice.call(arguments, 1));
 }
 
+//In ES6
+function add(...args) {
+    function parseArg(n) {
+        if (typeof n === 'function') return parseArg(n());
+        if (Array.isArray(n)) return add(...n);
+        return isNaN(n) ? 0 : parseInt(n, 10);
+    }
+    return args.length <= 1 ? parseArg(args[0]) : parseArg(args[0]) + add(args.slice(1));
+}
+
 add(10,20);
 add(10, '20');
 add(10, 'abc');
