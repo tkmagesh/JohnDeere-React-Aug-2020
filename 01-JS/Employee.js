c50347fe4e3fe86890ad89d897760ffecb8af3f7
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-17 17:01:46 +0530

    ES5 complete

--- 01-JS/Employee.js
+++ 01-JS/Employee.js
@@ -4,8 +4,9 @@ function Employee(id, name, salary) {
     this.id = id;
     this.name = name;
     this.salary = salary;
-
-    this.display = function () {
-        console.log(this.id, this.name, this.salary);
-    }
 }
+
+Employee.prototype.display = function () {
+    console.log(this.id, this.name, this.salary);
+}
+
