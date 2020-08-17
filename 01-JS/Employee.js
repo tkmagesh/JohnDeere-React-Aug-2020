5765ea812899c2b7c279d03e17c1924918124ff8
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-17 16:27:44 +0530

    constructor functions

---
+++ 01-JS/Employee.js
@@ -0,0 +1,11 @@
+function Employee(id, name, salary) {
+    if (this.constructor !== Employee)
+        return new Employee(id, name, salary);
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+
+    this.display = function () {
+        console.log(this.id, this.name, this.salary);
+    }
+}
