a73739cedf63b041f844fafa8ed170269a562f9d
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-21 12:37:59 +0530

    addAsyncPromise implementation

--- 06-async-demo/demo.js
+++ 06-async-demo/demo.js
@@ -13,20 +13,38 @@
     }
     window['addSyncClient'] = addSyncClient;
 
-    function addAsync(x, y) {
+    function addAsync(x, y, callback) {
         console.log(`   [@Service] processing ${x} and ${y}`);
         setTimeout(function(){
             const result = x + y;
             console.log(`   [@Service] returning the result`);
-            return result;
+            callback(result);
         }, 5000);
     }
 
     function addAsyncClient(x, y) {
         console.log(`[@Client] invoking the service`);
-        const result = addAsync(x, y);
-        console.log(`[@Client] result = ${result}`);
+        addAsync(x, y, function(result){
+            console.log(`[@Client] result = ${result}`);
+        });
     }
     window['addAsyncClient'] = addAsyncClient;
+
+
+    function addAsyncPromise(x, y) {
+        console.log(`   [@Service] processing ${x} and ${y}`);
+
+        const promise = new Promise(function(resolve, reject){
+            setTimeout(function () {
+                const result = x + y;
+                console.log(`   [@Service] returning the result`);
+                resolve(result);
+            }, 5000);
+        });
+        
+        return promise;
+    }
+
+    window['addAsyncPromise'] = addAsyncPromise;
 
 })();
