9bf3a6ac93d02f9a084667de48b3fd8a6786fffc
Magesh Kuppan <tkmagesh77@gmail.com>
2020-08-21 11:14:07 +0530

    data file for server communication

--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,6 +1,39 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers';
 
-const appStore = createStore(rootReducer);
+/* 
+function loggerMiddleware(store){
+    return function(next){
+        return function(action){
+            console.group(action.type);
+            console.group('Before');
+            console.log(store.getState());
+            console.groupEnd();
+            console.log(action);
+            next(action);
+            console.group('After');
+            console.log(store.getState());
+            console.groupEnd();
+            console.groupEnd();
+        }
+    }
+}
+
+*/
+
+const loggerMiddleware = store => next => action => {
+    console.group(action.type);
+    console.group('Before');
+    console.log(store.getState());
+    console.groupEnd();
+    console.log(action);
+    next(action);
+    console.group('After');
+    console.log(store.getState());
+    console.groupEnd();
+    console.groupEnd();
+};
+
+const appStore = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 export default appStore;
